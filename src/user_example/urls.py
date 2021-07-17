53fd28786a2f77d766cf7860bfbcf39fba88377f
Ata-92 <atakal92@gmail.com>
2021-07-17 19:52:19 +0300

    Add password change page

--- src/user_example/urls.py
+++ src/user_example/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
-from user_example.views import home_view, register, special
+from user_example.views import home_view, password_change, register, special
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home_view, name="home"),
     path('special/', special, name="special_view"),
     path('register/', register, name="register"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name="registration/password_change.html"), name="password_change")
 ]
