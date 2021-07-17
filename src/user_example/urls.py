9cd863eccc27f2367dc88e0cf19522ba498ebe25
Ata-92 <atakal92@gmail.com>
2021-07-17 19:08:25 +0300

    Add login-required decorator

--- src/user_example/urls.py
+++ src/user_example/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from user_example.views import home_view
+from user_example.views import home_view, special
 
 urlpatterns = [
     path('', home_view, name="home"),
+    path('special/', special, name="special_view"),
 ]
--- src/user_example/views.py
+++ src/user_example/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home_view(request):
     return render(request, "user_example/home.html")
+
+@login_required
+def special(request):
+    return render(request, "user_example/special.html")
